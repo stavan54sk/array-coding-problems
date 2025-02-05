e756268b45ce3ef9e42c360070f67c5329b18124
stavan54sk <stavan54sk@gmail.com>
2025-02-06 00:31:16 +0530

    2nd largest number in array

--- src/main/java/com/myarrays/App.java
+++ src/main/java/com/myarrays/App.java
@@ -4,23 +4,43 @@ package com.myarrays;
  * Hello world!
  *
  */
-public class App 
+public class App
 
 {
-	void largestNumber(int a[]){
-		int min=a[0];
+	void largestNumber(int a[]) {
+		int max = a[0];
 		for (int i = 1; i < a.length; i++) {
-			if(a[i]<min)
-				min =a[i];
+			if (a[i] > max)
+				max = a[i];
 		}
-		System.out.println(min);
-		
+		System.out.println(max);
+
 	}
-	
-	
-    public static void main( String[] args )
-    {
-    	new App().largestNumber(new int[] {11,12,34,45,56,67,89,1});;
+
+	void secondLargestNumber(int a[]) {
+		int max = a[0];
+		int smax = Integer.MIN_VALUE;
+		for (int i = 1; i < a.length; i++) {
+			int tsmax = Integer.MIN_VALUE;
+			if (a[i] > max) {
+				smax = max;
+				max = a[i];
+
+			}
+			if (a[i] < max) {
+				tsmax = a[i];
+			}
+			if(tsmax>smax) {
+				smax=tsmax;
+			}
+		}
+
+		System.out.println(smax);
+	}
+
+	public static void main(String[] args) {
+		new App().secondLargestNumber(new int[] { -12, -12, -1, -4, -5, -7, -9, -1 });
+		;
 //        System.out.println( "Hello World!" );
-    }
+	}
 }
